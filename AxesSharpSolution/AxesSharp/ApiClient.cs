18167f473c28e9a1babf4ac9589e53a53b9c34fc
Alan Alves Luiz <aaluiz@gmail.com>
2020-02-14 17:16:10 -0300

    primeira versão

---
+++ AxesSharpSolution/AxesSharp/ApiClient.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace AxesSharp
+{
+    public class ApiClient
+    {
+        public async Task<T> Get<T>(string url) where T : new()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            var response = await client.ExecuteAsync<T>(request);
+            return response.Data;
+        }
+
+        public async Task<T> Get<T>(string url, string token) where T : new()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", string.Format("Bearer {0}", token));
+            var response = await client.ExecuteAsync<T>(request);
+            return response.Data;
+        }
+
+        public async Task<T> Post<T>(string url, object body) where T: new()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+            var response = await client.ExecuteAsync<T>(request);
+            return response.Data;
+        }
+
+        public async Task<T> Post<T>(string url, object body, string token) where T : new()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+            request.AddHeader("Authorization", string.Format("Bearer {0}", token));
+            var response = await client.ExecuteAsync<T>(request);
+            return response.Data;
+        }
+
+        public async Task<T> Put<T>(string url, object body) where T : new()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+            var response = await client.ExecuteAsync<T>(request);
+            return response.Data;
+        }
+
+        public async Task<T> Put<T>(string url, object body, string token) where T : new()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+            request.AddHeader("Authorization", string.Format("Bearer {0}", token));
+            var response = await client.ExecuteAsync<T>(request);
+            return response.Data;
+        }
+
+        public async Task<T> Delete<T>(string url, object body) where T : new()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+            var response = await client.ExecuteAsync<T>(request);
+            return response.Data;
+        }
+
+        public async Task<T> Delete<T>(string url, object body, string token) where T : new()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+            request.AddHeader("Authorization", string.Format("Bearer {0}", token));
+            var response = await client.ExecuteAsync<T>(request);
+            return response.Data;
+        }
+    }
+}
